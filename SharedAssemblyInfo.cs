90c57b50a7497e9688361ddc4d8bfb844ac4de7a
Kamran Ayub <kamran.ayub@gmail.com>
2013-03-20 21:04:11 -0700

    Increment version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Resources;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
